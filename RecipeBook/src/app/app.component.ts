c6b72c5df20f17c3ff286f6fc2e2c2b27b1e178e
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-04-25 20:32:52 +0300

    implemented dropDown function

--- RecipeBook/src/app/app.component.ts
+++ RecipeBook/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  recipeClicked:boolean = false;
+  recipeClicked:boolean = true;
 
 
   onPropertyClicked(property: string){
