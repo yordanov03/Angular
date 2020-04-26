644ba56b258380112e26cdbb4f1a5543376e8f1d
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-04-26 14:45:54 +0300

    completed section 9

---
+++ services-start/src/app/account.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, EventEmitter } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable()
+export class AccountService{
+    accounts: {name:string, status: string} [] = [ {
+        name: 'Master Account',
+        status: 'active'
+      },
+      {
+        name: 'Testaccount',
+        status: 'inactive'
+      },
+      {
+        name: 'Hidden Account',
+        status: 'unknown'
+      }]
+  
+      constructor(private loggingService: LoggingService){}
+
+statusUpdated = new EventEmitter<string>();
+
+    addAccount(name: string, status: string){
+        this.accounts.push({name: name, status: status})
+        this.loggingService.logStatusChange(status);
+    }
+
+    updateStatus(id: number, status: string){
+        this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
+    }
+}
