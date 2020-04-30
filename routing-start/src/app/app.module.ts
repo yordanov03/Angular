c5c8cd4391c6907b672f6e7343aa631e905affb5
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-04-30 14:48:41 +0300

    routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+const routes: Routes=[
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
+  {path: 'servers', component: ServersComponent}
+]
 
 @NgModule({
   declarations: [
@@ -25,6 +33,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
