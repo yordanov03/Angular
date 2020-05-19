de1243488df062ef01f4eb248e44841fc41ab16c
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-05-19 09:53:36 +0300

    finished observables module

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated: boolean = false;
+  private activatedSub: Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate=>{
+      this.userActivated = didActivate
+    })
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe();
+  }
 }
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +13,33 @@ export class HomeComponent implements OnInit, OnDestroy {
  
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count)=>{
-      console.log(count);
-    })
+    // this.firstObsSubscription = interval(1000).subscribe((count)=>{
+    //   console.log(count);
+    // })
+
+    const customObservable = Observable.create(observer =>{
+      let count = 0;
+      setInterval(()=>{
+        observer.next(count);
+        if (count===5) {
+          observer.complete();
+        }
+
+        if(count>3){
+          observer.error(new Error('Greater than 3'));
+        }
+        count++
+      }, 1000)
+    });
+
+    
+
+    this.firstObsSubscription = customObservable.subscribe(data=>{
+      console.log(data)},
+      error=>{
+        console.log(error);
+        alert(error.message);
+    });
   }
 
   ngOnDestroy(): void {
