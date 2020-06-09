40ec04b79773369fdf0ec05f8121c7ec918c8da7
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-06-09 14:48:31 +0300

    completed TD assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+defaultOption="Advanced";
+formSubmitted = false;
+@ViewChild('f',{static: false}) signupForm: NgForm;
+user={
+  email:'',
+  level:'',
+  password:''
+}
 
+onSubmit(){
+  console.log(this.signupForm)
+this.formSubmitted=true;
+this.user.email=this.signupForm.value.userData.email;
+this.user.level=this.signupForm.value.userData.level;
+this.user.password = this.signupForm.value.userData.password;
+this.signupForm.reset();
+}
 }
