2e00dd8e6b602c363c0e8fc434a2931631b9bc19
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-06-13 17:53:25 +0300

    finished assignment

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+singupForm: FormGroup;
+statuses=['Stable', 'Critical', 'Finished']
+forbiddenProjectNames=['Test']
+
+ngOnInit(){
+
+  this.singupForm = new FormGroup({
+    'projectName': new FormControl(null, [Validators.required, this.forbiddenProjects.bind(this)]),
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'status': new FormControl('critical')
+  })
+}
+
+onSubmit(){
+  console.log(this.singupForm.value);
+}
+
+forbiddenProjects(control: FormControl): {[s: string]: boolean} {
+  if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+    return {'nameIsForbidden': true};
+  }
+  return null;
+}
 }
