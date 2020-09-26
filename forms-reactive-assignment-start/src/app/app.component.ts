2d609d48ae714602893476c6fd43d29c8024b7be
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-09-26 19:45:19 +0300

    modified component

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { resolve } from 'dns';
+
 
 @Component({
   selector: 'app-root',
