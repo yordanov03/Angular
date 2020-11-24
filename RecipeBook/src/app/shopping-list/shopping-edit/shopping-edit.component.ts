48364e13c52912791ac2f7a0ec22a94c808aa39a
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-11-24 09:50:03 +0200

    357 done

--- RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import * as ShoppingListActions from "../store/shopping-list.actions"
+import * as fromShoppingListActions from "../store/shopping-list.reducer"
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -21,23 +22,31 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f', { static: false }) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
-  editedItemIndex: number;
   editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService,
-    private store: Store<{ingredients: Ingredient[]}>) { }
+    private store: Store<fromShoppingListActions.AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('shoppingList').subscribe(stateData=>{
+      if(stateData.editedIngredientIndex>-1){
+        this.editMode = true
+        this.editedItem = stateData.editedIngredient
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+      }
+      else{
+        this.editMode = false;
+      }
+    })
     this.subscription = this.slService.startedEditing
       .subscribe(
         (index: number) => {
-          this.editedItemIndex = index;
           this.editMode = true;
           this.editedItem = this.slService.getIngredient(index);
-          this.slForm.setValue({
-            name: this.editedItem.name,
-            amount: this.editedItem.amount
-          })
+          
         }
       );
   }
@@ -46,7 +55,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient(newIngredient))
     } else {
       //this.slService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient))
@@ -58,15 +67,17 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onClear() {
     this.slForm.reset();
     this.editMode = false;
+    this.store.dispatch(new ShoppingListActions.StopEdit)
   }
 
   onDelete() {
-    this.slService.deleteIngredient(this.editedItemIndex);
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient())
     this.onClear();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+    this.store.dispatch(new ShoppingListActions.StopEdit)
   }
 
 }
--- RecipeBook/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-list.component.ts
@@ -4,6 +4,8 @@ import { Observable, Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import * as fromShoppingList from './store/shopping-list.reducer'
+import * as ShoppingListActions from './store/shopping-list.actions'
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,14 +16,15 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ingredients: Ingredient[]}>;
 
   constructor(private slService: ShoppingListService, 
-    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
+    private store: Store<fromShoppingList.AppState>
+    ) { }
 
   ngOnInit() {
     this.ingredients = this.store.select('shoppingList')
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index))
   }
 
   ngOnDestroy() {
