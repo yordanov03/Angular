dec1ebc0700fd0329f33f4b75986c4cb79c73ef7
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-12-08 19:54:36 +0200

    fixed names

--- RecipeBook/src/karma.conf.js
+++ RecipeBook/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ng-complete-guide-update'),
+      dir: require('path').join(__dirname, '../coverage/RecipeBook'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
